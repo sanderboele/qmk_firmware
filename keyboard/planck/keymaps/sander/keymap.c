790493a95518ceca91677f7fbfdc0e21839bfb94
Sander Boele <sander@boele.org>
2016-10-08 20:41:52 +0200

    new layer added

--- keyboard/planck/keymaps/sander/keymap.c
+++ keyboard/planck/keymaps/sander/keymap.c
@@ -23,6 +23,7 @@ extern keymap_config_t keymap_config;
 //#define _PLOVER 6
 #define _TENKEY 6
 #define _ADJUST 16
+#define _SANDER 15
 
 // Macro name shortcuts
 #define QWERTY M(_QWERTY)
@@ -30,6 +31,7 @@ extern keymap_config_t keymap_config;
 #define DVORAK M(_DVORAK)
 #define LOWER M(_LOWER)
 #define RAISE M(_RAISE)
+#define SANDER M(_SANDER)
 #define M_BL 5
 #define AUD_OFF M(6)
 #define AUD_ON M(7)
@@ -57,14 +59,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * |Sander| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  F(0)},
-  {M(M_BL), KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {SANDER, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Lower
@@ -102,6 +104,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
+
+/* My layer called "Sander" :)
+*
+*  CMD-backtick
+*
+*
+*
+*
+*
+*/
+
+[_SANDER] = {
+  {HYPR(KC_GRV), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+},
+
 
 
 /* My Ten Key layer repurposed PLOVER
@@ -352,6 +372,13 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         case 14:
           if (record->event.pressed) {
             return MACRO( D(E), D(R), D(F), D(V), D(O), D(L), U(E), U(R), U(F), U(V), U(O), U(L), END );
+          }
+        break;
+        case 15:
+          if (record->event.pressed) {
+            layer_on(_SANDER);
+          } else {
+            layer_off(_SANDER);
           }
         break;
       }
